9696abbf437e283e52075081bd73b025493464cf
Eliabe <eliabesantos1106@gmail.com>
2021-11-11 18:46:00 -0300

    update

--- back-end/go.mod
+++ back-end/go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 replace github.com/eliabe71/vida-saudavel/back-end/src/router => ./src
 
-require github.com/eliabe71/vida-saudavel/back-end/src/router v0.0.0-00010101000000-000000000000 // indirect
+require (
+	github.com/eliabe71/vida-saudavel/back-end/src/router v0.0.0-00010101000000-000000000000 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- back-end/src/router.go
+++ back-end/src/router.go
@@ -1,7 +1,27 @@
 package router
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
+func e(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		io.WriteString(w, "Hello from a HandleFunc #1!\n")
+	}
+
+	r.Response = new(http.Response)
+	r.Response.Status = "Error"
+	r.Response.StatusCode = 404
+	js, _ := json.Marshal(r.Response)
+
+	io.WriteString(w, string(js))
+
+}
 func Eliabe() {
-	fmt.Print("ELiabe")
+	http.HandleFunc("/c", e)
+	http.HandleFunc("/ca", e)
+	http.ListenAndServe(":8084", nil)
+
 }
